4eb006ec690339f8401730111c5ba07e7a31a055
Ben Saufley <bensaufley+git@gmail.com>
2021-03-05 00:09:29 -0500

    default root_dir in jest

--- spec/support/jest-setup.ts
+++ spec/support/jest-setup.ts
@@ -1,3 +1,6 @@
-process.env.NODE_ENV = 'test';
+import { resolve } from 'path';
+
 process.env.IMPORTS_PATH = '/imports';
+process.env.NODE_ENV = 'test';
+process.env.ROOT_DIR ||= resolve(__dirname, '../..');
 process.env.STORAGE_PATH = '/storage';
