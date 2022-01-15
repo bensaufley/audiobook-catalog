290ffb27ae9b83c1274d86115a58bcc471de33c7
Ben Saufley <bensaufley+git@gmail.com>
2022-01-14 23:22:08 -0500

    v0.1.0: it's styled, a little, and download links should be good

--- src/client/components/Books/index.tsx
+++ src/client/components/Books/index.tsx
@@ -1,14 +1,33 @@
-import { FunctionComponent, h } from 'preact';
-import { useEffect, useState } from 'preact/hooks';
+import { FunctionComponent, Fragment, h } from 'preact';
+import { useCallback, useEffect, useMemo, useState } from 'preact/hooks';
 
 import Book from '~client/components/Book';
 import type Audiobook from '~db/models/Audiobook';
 
 import styles from '~client/components/Books/styles.module.css';
+import BookModal from '~client/components/BookModal';
 
 const Books: FunctionComponent = () => {
   const [error, setError] = useState<string>();
   const [books, setBooks] = useState<Audiobook<unknown>[]>();
+  const [selectedBookId, setSelectedBookId] = useState<string>();
+
+  const selectedBook = useMemo(() => {
+    if (!selectedBookId) return null;
+
+    return books?.find(({ id }) => id === selectedBookId)!;
+  }, [books, selectedBookId]);
+
+  const showBook = useCallback(
+    (id: string) => {
+      setSelectedBookId(id);
+    },
+    [setSelectedBookId],
+  );
+
+  const hideBook = useCallback(() => {
+    setSelectedBookId(undefined);
+  }, [setSelectedBookId]);
 
   useEffect(() => {
     (async () => {
@@ -22,16 +41,28 @@ const Books: FunctionComponent = () => {
     })();
   }, []);
 
+  if (error) {
+    return (
+      <>
+        <h2>Error</h2>
+        <p>{error}</p>
+      </>
+    );
+  }
+
   if (!books) {
     return <h2>Loading&hellip;</h2>;
   }
 
   return (
-    <div class={styles.container}>
-      {books?.map((book) => (
-        <Book book={book} />
-      ))}
-    </div>
+    <>
+      <div class={styles.container}>
+        {books?.map((book) => (
+          <Book book={book} handleClick={showBook} />
+        ))}
+      </div>
+      {selectedBook && <BookModal book={selectedBook} handleHide={hideBook} />}
+    </>
   );
 };
 
