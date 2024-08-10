b04df305538fcdc796851dcf73276a5936ccd765
Ben Saufley <b.saufley@momentumcommerce.com>
2024-08-10 11:35:49 -0400

    Output coverage

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig, mergeConfig } from 'vitest/config';
+
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      coverage: {
+        provider: 'istanbul',
+        reporter: ['text', 'json', 'html'],
+      },
+    },
+  }),
+);
