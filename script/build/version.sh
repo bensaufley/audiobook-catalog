18a65846ed955d4226e9b9f27fbbd5d97c400ac7
Ben Saufley <bensaufley+git@gmail.com>
2024-09-08 17:55:01 -0400

    Update XML

--- script/build/version.sh
+++ script/build/version.sh
@@ -5,6 +5,6 @@ set -eo pipefail
 cd "$(dirname "$0")/../.."
 
 version="$(jq -r .version package.json)"
-current_version="$(grep -oP '<Version>\K[^<]+' community-applications.xml)"
+current_version="$(grep -oP 'bnsfly/audiobook-catalog:v\K[^<]+' community-applications.xml)"
 sed -i "s|${current_version//\./\\.}|$version|g" community-applications.xml
 git add community-applications.xml
