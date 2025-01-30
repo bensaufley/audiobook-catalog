50d42727ec80e4c1b3939dbc1d30e4866805fd1b
Ben Saufley <bensaufley+git@gmail.com>
2025-01-29 23:02:48 -0500

    rollback transaction on error

--- src/server/filesystem/importBook.ts
+++ src/server/filesystem/importBook.ts
@@ -44,78 +44,81 @@ const importBook = async (
       log.debug('parsed %s.', importFile);
 
       const transaction = await sequelize.transaction();
-
-      const name = title || album;
-      if (!name) {
-        log.error('No title for file %s. Skipping', importFile);
-        return format('No title for file %s. Skipping', importFile);
-      }
-      log.info('Importing %s...', name);
-
-      if (!authors?.length) throw new Error('Cannot import a book without authors');
-
-      const auths = authors.map((author) => {
-        const names = author.split(/ (?=[^ ]+$)/);
-        const lastName = names.pop()!;
-        const firstName = names[0] || null;
-        return { firstName, lastName };
-      });
-      const existingAuthors = await Author.findAll({ where: { [Op.or]: auths } });
-      const authorsToAdd = auths.filter(
-        (auth) => !existingAuthors.some((a) => a.firstName === auth.firstName && a.lastName === auth.lastName),
-      );
-      const addedAuthors = await Author.bulkCreate(authorsToAdd, { transaction });
-      const authorRecords = [...existingAuthors, ...addedAuthors];
-
-      const safeTitle = name.replace(/[/\\?%*:|"<>%]/g, '');
-      const authorsString = authorRecords
-        .toSorted((a, b) => a.lastName.localeCompare(b.lastName, undefined, { caseFirst: 'false' }))
-        .map((auth) => [auth.firstName, auth.lastName].join(' ').replace(/[/\\?%*:|"<>%]/g, ''))
-        .join(', ');
-      const dir = `/audiobooks/${authorsString}/${safeTitle}`;
-      const filename = `${safeTitle}${extname(importFile)}`;
-      const filepath = `${dir}/${filename}`;
-
-      let narratorRecords: Narrator[] = [];
-
-      if (narrators?.length) {
-        let nars = narrators;
-        if (nars.length === 1) {
-          nars = nars[0]!.split(/,(?! *(?:jr\.?|sr\.?|junior|senior|i+v?|vi+)\b)/i);
+      try {
+        const name = title || album;
+        if (!name) {
+          log.error('No title for file %s. Skipping', importFile);
+          return format('No title for file %s. Skipping', importFile);
         }
+        log.info('Importing %s...', name);
 
-        const narrs = nars.map((narrator) => {
-          const names = narrator.split(/ (?=[^ ]+$)/);
+        if (!authors?.length) throw new Error('Cannot import a book without authors');
+
+        const auths = authors.map((author) => {
+          const names = author.split(/ (?=[^ ]+$)/);
           const lastName = names.pop()!;
           const firstName = names[0] || null;
           return { firstName, lastName };
         });
-        const existingNarrators = await Narrator.findAll({ where: { [Op.or]: narrs } });
-        const narratorsToAdd = narrs.filter(
-          (auth) => !existingNarrators.some((a) => a.firstName === auth.firstName && a.lastName === auth.lastName),
+        const existingAuthors = await Author.findAll({ where: { [Op.or]: auths } });
+        const authorsToAdd = auths.filter(
+          (auth) => !existingAuthors.some((a) => a.firstName === auth.firstName && a.lastName === auth.lastName),
         );
-        const addedNarrators = await Narrator.bulkCreate(narratorsToAdd, { transaction });
-        narratorRecords = [...existingNarrators, ...addedNarrators];
+        const addedAuthors = await Author.bulkCreate(authorsToAdd, { transaction });
+        const authorRecords = [...existingAuthors, ...addedAuthors];
+
+        const safeTitle = name.replace(/[/\\?%*:|"<>%]/g, '');
+        const authorsString = authorRecords
+          .toSorted((a, b) => a.lastName.localeCompare(b.lastName, undefined, { caseFirst: 'false' }))
+          .map((auth) => [auth.firstName, auth.lastName].join(' ').replace(/[/\\?%*:|"<>%]/g, ''))
+          .join(', ');
+        const dir = `/audiobooks/${authorsString}/${safeTitle}`;
+        const filename = `${safeTitle}${extname(importFile)}`;
+        const filepath = `${dir}/${filename}`;
+
+        let narratorRecords: Narrator[] = [];
+        if (narrators?.length) {
+          let nars = narrators;
+          if (nars.length === 1) {
+            nars = nars[0]!.split(/,(?! *(?:jr\.?|sr\.?|junior|senior|i+v?|vi+)\b)/i);
+          }
+
+          const narrs = nars.map((narrator) => {
+            const names = narrator.split(/ (?=[^ ]+$)/);
+            const lastName = names.pop()!;
+            const firstName = names[0] || null;
+            return { firstName, lastName };
+          });
+          const existingNarrators = await Narrator.findAll({ where: { [Op.or]: narrs } });
+          const narratorsToAdd = narrs.filter(
+            (auth) => !existingNarrators.some((a) => a.firstName === auth.firstName && a.lastName === auth.lastName),
+          );
+          const addedNarrators = await Narrator.bulkCreate(narratorsToAdd, { transaction });
+          narratorRecords = [...existingNarrators, ...addedNarrators];
+        }
+
+        const audiobook = await Audiobook.create(
+          {
+            cover: cover ? Buffer.from(cover) : null,
+            coverType,
+            filepath,
+            title: name,
+            duration: Number.isNaN(duration) ? null : duration,
+            Authors: authorRecords,
+            Narrators: narratorRecords,
+          },
+          { transaction },
+        );
+
+        await transaction.commit();
+
+        log.info('Done importing %s.', name);
+
+        return audiobook;
+      } catch (err) {
+        await transaction.rollback();
+        throw err;
       }
-
-      const audiobook = await Audiobook.create(
-        {
-          cover: cover ? Buffer.from(cover) : null,
-          coverType,
-          filepath,
-          title: name,
-          duration: Number.isNaN(duration) ? null : duration,
-          Authors: authorRecords,
-          Narrators: narratorRecords,
-        },
-        { transaction },
-      );
-
-      await transaction.commit();
-
-      log.info('Done importing %s.', name);
-
-      return audiobook;
     }, 5);
 
     return resp;
